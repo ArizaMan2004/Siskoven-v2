4bd53d28b4a2bf48e7a0076b7d92b8415674a26a
Arizaman2004 <jesussdk2004@gmail.com>
2025-11-05 18:34:00 -0400

    actualizacion

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,9 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Siskoven - Sistema de Inventario",
+  description: "Impulsa tu negocio nuestro innvovador sistema de gestion de inventario",
+  generator: "Jesus Ariza",
 }
 
 export default function RootLayout({
